7ff373c9529c0d1a0debefaa6c8a3851de1b8689
许力扬 <xuleeyoung@sjtu.edu.cn>
2024-10-04 18:45:39 -0400

    initial

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+  extends: ["airbnb", "plugin:cypress/recommended", "prettier"],
+  plugins: ["react", "react-hooks", "jsx-a11y", "import"],
+  env: {
+    browser: true,
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    // Allow use of console.log()
+    "no-console": 0,
+
+    // Rules for React Hooks
+    "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
+    "react-hooks/exhaustive-deps": "error", // Checks effect dependencies
+
+    // A specific rule for Cypress tests that disallows committing code with cy.pause()
+    "cypress/no-pause": "error",
+  },
+  overrides: [
+    {
+      files: ["**/*.ts", "**/*.tsx"],
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+        ecmaVersion: "latest",
+        sourceType: "module",
+        project: "./tsconfig.json",
+      },
+      extends: ["airbnb", "airbnb-typescript", "prettier"],
+      plugins: ["react", "react-hooks", "jsx-a11y", "import"],
+      rules: {
+        "no-console": 0,
+        "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
+        "react-hooks/exhaustive-deps": "warn", // Checks effect dependencies
+      },
+    },
+  ],
+};
